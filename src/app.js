a62afbb5874be648769e094b170c98a6cbb6bcf0
Karla Sierra <ksierra@ucol.mx>
2022-11-18 14:18:58 -0600

    Actualizacion

--- src/app.js
+++ src/app.js
@@ -7,28 +7,19 @@ import { createPool } from "mysql2/promise";
 const pool=createPool({
 
     user:'root',
-
-    password:'aK0L1lQCoegrHGozc9L9',
-
-    host:'containers-us-west-86.railway.app',
-
-    port:5640,
-
+    password:'2qq3K4NB7F1c7hGdp66b',
+    host:'containers-us-west-118.railway.app',
+    port:6692,
     database:'railway'
-
 })
 
 const app=express()
-
- 
 
 app.get('/',(req,res)=>{
 
   res.send("Bienvenido a este servidor..")      
 
 })
-
- 
 
 app.get('/usuarios',async (req,res)=>{
 
@@ -39,10 +30,9 @@ app.get('/usuarios',async (req,res)=>{
   })
 
  
-
   app.get('/agregarusuario',async (req,res)=>{
 
-    const nombre=req.query.nombre
+    const nombre=req.query.no
 
     const contrasena=req.query.contrasena
 
@@ -50,14 +40,13 @@ app.get('/usuarios',async (req,res)=>{
 
     const tienda=req.query.tienda  
 
-    const [result]=await pool.query(`INSERT INTO usuario (nombre, contrasena, correo, tienda) VALUES ('${nombre}', '${contrasena}', '${correo}','${tienda}')`)
+    const [result]=await pool.query(`INSERT INTO usuario (no, contrasena, correo, tienda) VALUES ('${no}', '${contrasena}', '${correo}','${tienda}')`)
 
     res.json(result[0])      
 
   })
 
  
-
 app.listen(process.env.PORT || 3000)
 
 console.log("Servidor corriendo en el puerto 3000")
